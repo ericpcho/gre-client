ce6c701748fa671aaad16a624da4dc2b758a07ce
Eric Pcholinski <ericpcho@gmail.com>
2020-08-17 13:19:00 -0700

    imported redux middleware

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './reducer/reducer.js';
+import thunk from 'redux-thunk';
+
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
