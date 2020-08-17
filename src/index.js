31fd2c9dca0de077436f1ca0960f5c61e43af0ef
Eric Pcholinski <ericpcho@gmail.com>
2020-08-17 13:10:16 -0700

    added redux and store middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { applyMiddleware } from 'redux';
+
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
